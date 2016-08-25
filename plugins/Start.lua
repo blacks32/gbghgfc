a7c6ff7deed62bebfee1040793e59df295a5e92b
master00041 <master041amin@gmail.com>
2016-08-25 16:07:29 +0430

    Update Start.lua

--- plugins/Start.lua
+++ plugins/Start.lua
@@ -13,7 +13,7 @@ do
 
 function run(msg, matches)
 local reply_id = msg['id']
-local text = 'Welcome to BlackPlus!\n\n>To get a new BlackPlus group, contact a support group:\n https://telegram.me/joinchat/A2ETvD160xvsnwhJ3eQk0Q\n\nFor more information, check out our channels:\n\nChannel : @Black_CH\nSite : BlackPlus.ir \n\nUse #superhelp command to show bot commands!!\n\n#Thanks_for_using @BlackPlus!'
+local text = 'Welcome to MaXiMuS!\n\n>To get a new maximus group, contact @pedaret or @black_hc \n\nUse #superhelp command to show bot commands!!\n\n#Thanks_for_using @Xxmaximus_botxX!'
 if matches [1] == 'start' then
 reply_msg(reply_id, text, ok_cb, false)
 end
